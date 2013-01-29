5f63dd4b0c77b28f54f2a55f6dae1119cb0e8a4a
cmunozgar <cmunozgar@gmail.com>
2013-01-29 21:16:42 +0100

    add custom marker google maps

--- app/assets/javascripts/breweries.js
+++ app/assets/javascripts/breweries.js
@@ -7,15 +7,25 @@ function showMap(lat, long) {
     };
     var map = new google.maps.Map(document.getElementById("brew_map"), myOptions);
 
+    var iconBase = 'http://localhost:3000/assets/';
+
+    var iconShadow = new google.maps.MarkerImage(
+        iconBase + 'map_marker_shadow.png',
+        null,
+        null,
+        new google.maps.Point(2, 40)
+    );
+
     var marker = new google.maps.Marker({
         position: latlng,
-        map: map
+        map: map,
+        icon: iconBase + 'map_marker.png',
+        shadow: iconShadow
     });
 
 }
 
 $(function(){
-    alert('ashfklsaf');
     var lat = $('#brew_lat').val();
     var lgt = $('#brew_lgt').val();
     if (lat && lgt)
